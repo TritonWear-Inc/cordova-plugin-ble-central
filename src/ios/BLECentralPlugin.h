c8c95290dde9394dd51d1072cec0c0906f313f02
Vincent Wilkie <vincent@tritonwear.com>
2025-10-15 02:02:35 -0400

    disconnect wip

--- src/ios/BLECentralPlugin.h
+++ src/ios/BLECentralPlugin.h
@@ -29,6 +29,7 @@
     NSString* discoverPeripheralCallbackId;
     NSString* stateCallbackId;
     NSMutableDictionary* connectCallbacks;
+    NSMutableDictionary* disconnectCallbacks;
     NSMutableDictionary *readCallbacks;
     NSMutableDictionary *writeCallbacks;
     NSMutableDictionary *notificationCallbacks;
